7e77d47d31e7c0d0faa54a7afeca19a0a66e722c
Anup Ashtankar <122741245+anpia22@users.noreply.github.com>
2023-08-30 14:48:51 +0530

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-
   const [movies, setMovies] = useState([]);
   const [loadingData, setLoadingData] = useState(false);
-  const fatchMoviesHandler = async () => {
+  const fatchMoviesHandler = useCallback(async () => {
     setLoadingData(true);
     try {
-      const response = await fetch('https://swapi.dev/api/films')
+      const response = await fetch("https://swapi.dev/api/films");
       const data = await response.json();
 
-      const transformedMovies = data.results.map(movieData => {
+      const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
           openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
-      setMovies(transformedMovies)
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoadingData(false);
     }
-
-    catch (error) {
-      alert(error)
-    }
-    setLoadingData(false)
-  }
+  }, []);
+  useEffect(() => {
+    fatchMoviesHandler();
+  }, [fatchMoviesHandler]);
 
   return (
     <React.Fragment>
       <section>
-
         <button onClick={fatchMoviesHandler}>Fetch Movies</button>
       </section>
 
       <section>
-        { loadingData && <div className='loader'>
-          <h3>Loading</h3>
-        </div>}
+        {loadingData && (
+          <div className="loader">
+            <h3>Loading</h3>
+          </div>
+        )}
         <MoviesList movies={movies} />
       </section>
     </React.Fragment>
